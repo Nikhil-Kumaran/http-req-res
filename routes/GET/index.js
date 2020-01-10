efac9108fac84c56290f29aa50c27cc717d8198f
Nikhil-Kumaran <nikhil.kumaran.2011@gmail.com>
2020-01-10 23:25:33 +0530

    used fs to get JSON data instead of require

--- routes/GET/index.js
+++ routes/GET/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 
-const users = require('../../data/users')
+const fs = require('fs')
+const path = require('path')
+
+const usersPath = path.join(__dirname, '..', '..', 'data', 'users.json')
 
 const router = express.Router()
 
@@ -9,10 +12,12 @@ router.get('/', (req, res) => {
 })
 
 router.get('/users', (req, res) => {
+  const users = JSON.parse(fs.readFileSync(usersPath))
   res.status(200).send(users)
 })
 
 router.get('/users/:id', (req, res) => {
+  const users = JSON.parse(fs.readFileSync(usersPath))
   const user = users.filter(user => user.id === parseInt(req.params.id))
   if (user.length === 0) {
     res.status(404).send()
